98c15423664dce17a1d88c68664d3f57b18e59a6
Jakub Brehuv <kubek.breha@gmail.com>
2018-02-26 18:44:47 +0100

    keep up added

--- Assets/img/profile_image_files/cmbv2Mm8eLX.js
+++
@@ -1,4 +0,0 @@
-if (self.CavalryLogger) { CavalryLogger.start_js(["RDZPz"]); }
-
-__d("ComposerXSessionIDs",["cx","DOM","uuid"],(function a(b,c,d,e,f,g,h){var i={},j={getSessionID:function k(l){return i[l]},resetSessionID:function k(l){i[l]=c("uuid")()},createSessionIDInput:function k(l){return c("DOM").create("input",{type:"hidden",name:"composer_session_id",className:"_5r_b",value:l})}};f.exports=j}),null);
-__d("legacy:ScrollableArea",["ScrollableArea"],(function a(b,c,d,e,f,g){b.ScrollableArea=c("ScrollableArea")}),3);
