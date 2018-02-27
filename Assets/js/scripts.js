3661313227605fb96d4e7b5b56b704cc3a6b1c98
Jakub Brehuv <kubek.breha@gmail.com>
2018-02-27 16:44:14 +0100

    daphne finnished

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -8,3 +8,8 @@ $(document).ready(function(){
     $('.parallax').parallax();
 });
 
+$("show_work").click(function() {
+    $('html,body').animate({
+            scrollTop: $(".scroll_work").offset().top},
+        'slow');
+});
