5c556f32952be3398a438f67417871bc4cb8fe40
Jakub Brehuv <kubek.breha@gmail.com>
2018-03-04 12:49:27 +0100

    form added

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -17,3 +17,39 @@ $("show_work").click(function() {
 $(document).ready(function(){
     $('.materialboxed').materialbox();
 });
+
+//forms input
+$('#textarea1').val('New Text');
+$('#textarea1').trigger('autoresize');
+
+
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+});
+
+$('.timepicker').pickatime({
+    default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+    fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+    twelvehour: false, // Use AM/PM or 24-hour format
+    donetext: 'OK', // text for done-button
+    cleartext: 'Clear', // text for clear-button
+    canceltext: 'Cancel', // Text for cancel-button
+    autoclose: false, // automatic close timepicker
+    ampmclickable: true, // make AM PM clickable
+    aftershow: function(){} //Function for after opening timepicker
+});
+
+
+
+
+$('.chips').material_chip();
+
+$('.chips-placeholder').material_chip({
+    placeholder: 'Enter a tag',
+    secondaryPlaceholder: '+Tag',
+});
