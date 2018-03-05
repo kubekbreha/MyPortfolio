806661cafac0b20ddc9edfc8cab444a83fac0b85
Jakub Brehuv <kubek.breha@gmail.com>
2018-03-05 23:20:16 +0100

    google sign in added

--- Assets/js/firebase/firebase_login.js
+++ Assets/js/firebase/firebase_login.js
@@ -1,3 +1,8 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+
+
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         window.location.href = "admin.html";
@@ -21,4 +26,27 @@ function logIn() {
 
 function openRegister() {
     window.location.href = "admin_register.html";
+}
+
+
+function callGoogleSignIn(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+
+        document.getElementById("error_message").innerHTML = errorMessage;
+
+    });
 }
