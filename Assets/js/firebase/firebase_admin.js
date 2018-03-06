a9423586ea67a3be43bf4c4110016edeb753c2db
Jakub Brehuv <kubek.breha@gmail.com>
2018-03-06 11:26:33 +0100

    pushing to database from add article and settings

--- Assets/js/firebase/firebase_admin.js
+++ Assets/js/firebase/firebase_admin.js
@@ -11,20 +11,57 @@ function logout(){
 }
 
 
-// var articleText = document.getElementById("article_text");
-// var submitButton = document.getElementById("submit");
-// var testHeading = document.getElementById("h1_test");
-//
-// // var firebaseHeadingRef = firebase.database().ref().child("Heading");
-//
-// // firebaseHeadingRef.on('value', function (datasnapshot) {
-// //     testHeading.innerText = datasnapshot.val();
-// // });
-//
-// function submitClick() {
-//     var firebaseRef = firebase.database().ref();
-//
-//     var articleTextString = articleText.value;
-//
-//     firebaseRef.child("Articles").push().set(articleTextString);
-// }
+
+
+//-------------------------------push data to database-------------------------------
+function submitClick() {
+    var uid = firebase.auth().currentUser.uid;
+    var database = firebase.database();
+    var refUser = database.ref('users').child(uid);
+    var refArticles = database.ref('users').child(uid).child('articles');
+
+
+    var articleDate = document.getElementById("input_date");
+    var articleTime = document.getElementById("input_time");
+    var articleName = document.getElementById("input_article_name");
+    var articleText = document.getElementById("input_article_text");
+    //var articleImage = document.getElementById("input_article_image");
+    //var articleImageUri = document.getElementById("input_image_url");
+
+
+
+    var articleDateString = articleDate.value;
+    var articleTimeString = articleTime.value;
+    var articleNameString = articleName.value;
+    var articleTextString = articleText.value;
+
+
+    var data = {
+        articleDate: articleDateString,
+        authorTime: articleTimeString,
+        authorName: articleNameString,
+        authorText: articleTextString
+    };
+    refArticles.push(data);
+
+    window.alert("submit");
+}
+
+
+function setUserData(){
+    var uid = firebase.auth().currentUser.uid;
+    var database = firebase.database();
+    var refUser = database.ref('users').child(uid).child("userInfo");
+
+    var authorName = document.getElementById("input_name").value;
+    var authorEmail = document.getElementById("input_email").value;
+
+    window.alert("submit");
+
+    var userData = {
+        userName: authorName,
+        userEmail: authorEmail
+    };
+
+    refUser.set(userData);
+}
