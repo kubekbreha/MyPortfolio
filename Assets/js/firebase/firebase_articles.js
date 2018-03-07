615d95ab7eeb845e73de254e5dc9ddb4097d82db
Jakub Brehuv <kubek.breha@gmail.com>
2018-03-07 14:39:15 +0100

    dynamicaly updating, dynamic data texts bugged

--- Assets/js/firebase/firebase_articles.js
+++ Assets/js/firebase/firebase_articles.js
@@ -19,7 +19,6 @@ function loadArticles() {
 }
 
 function gotData(data) {
-
     var ul = document.getElementById("articles_list");
     while (ul.firstChild) ul.removeChild(ul.firstChild);
 
@@ -28,29 +27,30 @@ function gotData(data) {
     var keys = Object.keys(articles);
     console.log(keys);
     for (var i = 0; i < keys.length; i++) {
-        var k = keys[i];
-        var name = articles[k].articleName;
-        var text = articles[k].articleText;
-        var date = articles[k].articleDate;
-        var time = articles[k].articleTime;
-        var imageStored = articles[k].articleFileName;
+        var name = articles[keys[i]].articleName;
+        var text = articles[keys[i]].articleText;
+        var date = articles[keys[i]].articleDate;
+        var time = articles[keys[i]].articleTime;
+        var imageStored = articles[keys[i]].articleFileName;
 
-        // showimage(imageStored);
+        var uid = firebase.auth().currentUser.uid;
 
-        $('#cards_area').append('        <div class="col l6 s12 m12">\n' +
-            '            <div class="card">\n' +
-            '                <div class="card-image">\n' +
-            '                    <a href="daphne.html"><img title="Daphne article" src="Assets/img/daphne/expo.JPG"\n' +
-            '                                               alt="daphne project"></a>\n' +
-            '                    <span class="card-title">Daphne</span>\n' +
-            '                </div>\n' +
-            '                <div class="card-content">\n' +
-            '                    <p>Project Daphne is about finding a way to detect parkinson disease.\n' +
-            '                        Take a look how I helped on this project.</p>\n' +
-            '                </div>\n' +
-            '            </div>\n' +
-            '        </div>\n');
+        var storageRef = firebase.storage().ref();
 
+        storageRef.child("articleImages/" + uid + "/" + imageStored).getDownloadURL().then(function (url) {
+            $('#cards_area').append('        <div class="col l6 s12 m12">\n' +
+                '            <div class="card">\n' +
+                '                <div class="card-image">\n' +
+                '                    <a href=""><img title="' + name + '" src="' + url + '"\n' +
+                '                                               alt="' + name + '"></a>\n' +
+                '                    <span class="card-title">'  +name+ '</span>\n' +
+                '                </div>\n' +
+                '                <div class="card-content">\n' +
+                '                    <p>' + text + '</p>\n' +
+                '                </div>\n' +
+                '            </div>\n' +
+                '        </div>\n');
+        });
 
 
         var node = document.createElement("LI");
@@ -63,22 +63,4 @@ function gotData(data) {
 function errData(err) {
     console.log('Error!');
     console.log(err);
-}
-
-
-function showimage(imageStored) {
-    var storageRef = firebase.storage().ref();
-
-    var uid = firebase.auth().currentUser.uid;
-    var spaceRef = storageRef.child("articleImages/" + uid + "/");
-    spaceRef.child(imageStored).getDownloadURL().then(function (url) {
-        var test = url;
-        alert(url);
-        document.querySelector('img').src = test;
-
-    }).catch(function (error) {
-
-    });
-
-
 }
