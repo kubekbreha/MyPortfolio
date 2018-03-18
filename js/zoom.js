4f21990cb1c767729ef5a8692ecbd23664d75758
kubekbreha <kubek.breha@gmail.com>
2018-03-18 20:51:39 +0100

    zoom add

---
+++ js/zoom.js
@@ -0,0 +1,23 @@
+var ClassNameCary = "";
+
+function zoomPage(ClassNameCary){
+    var size = parseFloat(document.body.style.fontSize);
+    if(size < "30"){
+        document.body.style.fontSize = "30px";
+        document.body.style.color = "red";
+        var x = document.getElementsByClassName(ClassNameCary);
+        var i;
+        for(i = 0; i < x.length; i++){
+            x[i].style.color = "blue";
+        }
+    }
+    else {
+        document.body.style.fontSize = "20px";
+        document.body.style.color = "black";
+        var x = document.getElementsByClassName(ClassNameCary);
+        var i;
+        for(i = 0; i < x.length; i++){
+            x[i].style.color = "black";
+        }
+    }
+}
