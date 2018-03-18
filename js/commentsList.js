3a475d3a0072f60f63fb92df12fa4776079d600a
kubekbreha <kubek.breha@gmail.com>
2018-03-18 09:20:51 +0100

    shownig comments core done

---
+++ js/commentsList.js
@@ -0,0 +1,24 @@
+console.log("Downloading comments ...");
+var artId = queryString2obj().id;
+
+writeComment2Html(0, commentsForPage, server, 'komentare');
+
+
+function writeComment2Html(startIndex, max, server, commentElID){
+    $.ajax({
+        type: 'GET',
+        url: "http://"+server+"/api/article/"+artId+"/comment",
+        data: { max: max, offset: startIndex },
+        dataType: "json",
+        success: function (articles) {
+            $.get("templates/listOfComments.mst",
+                function (template) {
+                    $("#"+commentElID).html(Mustache.render(template, articles));
+                }
+                ,"text");
+        },
+        error:function(jxhr){
+            errorAlert("Comment loading failed.",jxhr);
+        }
+    });
+}
