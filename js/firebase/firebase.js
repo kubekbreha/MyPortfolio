3d22c6976e9e79fefbb8a83c199f653b265fcead
kubekbreha <kubek.breha@gmail.com>
2018-03-24 12:23:08 +0100

    nav bar, comments fix

--- js/firebase/firebase.js
+++ js/firebase/firebase.js
@@ -1,54 +1,53 @@
 var logged;
 
 //check if user is logged
-firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-        logged = true;
-        document.getElementById("log-nav").textContent = "LogIn";
-        document.getElementById("log-side-nav").textContent = "LogIn";
-        document.getElementById("log-out-invisible-add-project").style.display = "block";
-        Materialize.toast("Logged", 4000);
-    } else {
-        logged = false;
-        document.getElementById("log-nav").textContent = "LogOut";
-        document.getElementById("log-side-nav").textContent = "LogOut";
-        document.getElementById("log-out-invisible-add-project").style.display = "none";
-        Materialize.toast("NotLogged", 4000);
+$(document).ready(function () {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            logged = true;
+            document.getElementById("log-out-invisible-add-project").style.display = "block";
+            Materialize.toast("Logged", 4000);
+        } else {
+            logged = false;
+            document.getElementById("log-out-invisible-add-project").style.display = "none";
+            Materialize.toast("NotLogged", 4000);
 
-    }
+        }
+    });
 });
 
 //logout button
 function logOutOrIn() {
-    if(logged){
+    if (logged) {
         firebase.auth().signOut();
-    }else{
+    } else {
         window.location.href = "login.html";
     }
 }
+
 console.log("This is url : " + window.location.href);
 
-function openPage(url){
-    if(logged){
+function openPage(url) {
+    if (logged) {
         window.location.href = url;
-    }else{
+    } else {
         var userURL = window.location.href;
         var reg = /[^?]*$/g;
         var matches = userURL.match(reg);
         console.log("This is url matched: " + matches[0]);
         console.log("Parameter: " + url);
-        console.log("Redirect to: "+ url + "?" + matches);
+        console.log("Redirect to: " + url + "?" + matches);
 
-        window.location.href = url + "?" + matches[0] ;
+        window.location.href = url + "?" + matches[0];
     }
 }
 
 
-function openPageArticle(url){
-    if(logged){
+function openPageArticle(url) {
+    if (logged) {
         window.location.href = url;
-    }else{
+    } else {
         var userName = document.getElementById('user-name-template').textContent;
-        window.location.href = url + "?" + userName ;
+        window.location.href = url + "?" + userName;
     }
 }
--- js/navigation_scripts.js
+++ js/navigation_scripts.js
@@ -2,4 +2,17 @@ $(document).ready(function () {
     $('.materialboxed').materialbox();
     $('.scrollspy').scrollSpy();
     $(".button-collapse").sideNav();
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            document.getElementById("log-nav").textContent = "LogOut";
+            document.getElementById("log-side-nav").textContent = "LogOut";
+        } else {
+            document.getElementById("log-nav").textContent = "LogIn";
+            document.getElementById("log-side-nav").textContent = "LogIn";
+        }
+    });
+
+
 });
+
